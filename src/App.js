297524ae994114d2355890ba67ca2937c397df02
TeddyS26 <tsann010@uottawa.ca>
2023-06-09 01:53:43 -0400

    fix link

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
